9c53f76ea81c54e9ccc7defadf0a85e79359632a
Juan Diego Moreno <jdmorenoc@ufpso.edu.co>
2024-03-01 11:31:04 -0500

    Creacion de clase interna

--- POO/src/main/java/com/mycompany/poo/POO.java
+++ POO/src/main/java/com/mycompany/poo/POO.java
@@ -1,16 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.poo;
 
-/**
- *
- * @author SCIS2PC30
- */
 public class POO {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       Character homero = new Character("Homero", "Inspector de seguridad", 38, "A la grande le puse cuca");
+       homero.greeting();
+       
+       Character bart = new Character("Bartolomeo", "Vago", 10, "Ayyy caramba");
+       bart.greeting();
     }
+    
+    static class Character {
+       String name;
+       String ocupation;
+       int age;
+       String phrase;
+
+        public Character(String name, String ocupation, int age, String phrase){
+            this.name = name;
+            this.ocupation = ocupation;
+            this.age = age;
+            this.phrase = phrase;
+        }
+       
+        
+        
+       void greeting(){
+           System.out.println(this.phrase);
+       }
+    }   
 }
